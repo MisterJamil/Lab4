f85049e41328aa94319f68c031c187cc046faf99
Jamil <mr.jamilsaliba@gmail.com>
2016-09-26 14:37:28 -0700

    Added else statement
    Testing out else statement for integer values. Added paramters for
    invlaid values.

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -20,18 +20,19 @@ int main()
 	//Input
 	printf("Enter a postive nonzero integer between 1-100>\n");
 	scanf("%d", &iNumber);
-	while (iNumber != 0) {
+	while (iNumber == 2, 3, 5, 7, 11, 13, 17, 19, 23, 29, 31, 37, 41, 43, 47, 53, 59, 61, 67, 71, 73, 79, 83, 89, 97) {
 		iPrime = iNumber / 1;
 		printf("Value is a prime number");
 
 	}
- else {
-
-	 //Output
+ else { 
+	 iNumber == 4, 6, 8, 9, 10, 12, 14, 15, 16, 18, 20, 21, 22, 24, 25, 26, 27, 28, 30,
+		 32, 33, 34, 35, 36, 38, 39, 40, 42, 44, 45, 46, 48, 49, 50, 51, 52, 54,
+		 55, 56, 57, 58, 60, 62, 63, 64, 65, 66, 68, 69, 70, 72, 74, 75, 76, 77,
+		 78, 80, 81, 82, 84, 85, 86, 87, 88, 90, 91, 92, 93, 94, 95, 96, 98, 99, 100;
 	 printf("Value is not a prime number");
+	 iNumber <= 0, >=100;
 	 printf("Error: Invalid value");
-
-	 //Calculations
  }
 	return 0;
 }
