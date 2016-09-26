453835baa0bbbba28bd7258e91aee0eb25d46ed8
Jamil <mr.jamilsaliba@gmail.com>
2016-09-26 13:43:09 -0700

    Initial commit
    Creating a console app that finds prime numbers using a loop function.
    The user inputs a positive nonzero integer between 1- 100, if the value
    is a prime number it is displayed. If the value is an unvalid value and
    error message is displayed.

--- Lab4/Lab4.cpp
+++ Lab4/Lab4.cpp
@@ -1,5 +1,13 @@
 // Lab4.cpp : Defines the entry point for the console application.
-//
+/* Author: Jamil Saliba
+	Date : 9/26/2016
+	Description : Console application that checks if a valid value is a prime number and then displays if it is a prime number or not. If an unvalid value is inputed then
+	an error message is displayed.
+
+	Finding a prime. Write a program that asks the user for a positive nonzero integer value up to 100. The program should use a loop to determine if the number entered is 
+	prime or not and display the result back to the user. If the user enters an invalid number, display error message and end the program.
+
+*/
 
 #include "stdafx.h"
 
